7be45aed37848f022bf04f3b4176d0582c049a1d
bhavya singh <samgta9810@gmail.com>
2025-06-21 13:25:24 +0530

    initial commit, general apis tried to create

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,50 @@
+package net.engineeringdigest.journalApp.controller;
+
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long,JournalEntry> journalEntries=new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+
+        journalEntries.put(myEntry.getId(), myEntry);
+
+        return true;
+
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+    return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public boolean deleteJournalEntryById(@PathVariable Long myId){
+
+      return journalEntries.remove(myId)!=null;
+
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalById(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
+
+       return  journalEntries.put(myId,myEntry);
+    }
+
+
+}
